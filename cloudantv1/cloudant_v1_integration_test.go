590de77313eca3dc4896b08f5cff96d88452a6ab
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2025-01-08 12:17:04 +0000

    chore(generated): 2025 updates
    Generated SDK source code using:
    - Generator version 3.98.0
    - Specification version 1.0.0-dev0.1.23
    - Automation (cloudant-sdks) version c1e4608

--- cloudantv1/cloudant_v1_integration_test.go
+++ cloudantv1/cloudant_v1_integration_test.go
@@ -1,7 +1,7 @@
 //go:build integration
 
 /**
- * (C) Copyright IBM Corp. 2024.
+ * (C) Copyright IBM Corp. 2025.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
